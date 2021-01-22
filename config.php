904a3f30fe769f445726ceea1e76df2433cba0f4
rg959 <rayan.galouzi@gmail.com>
2021-01-22 10:14:44 +0100

    deuxième commit

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ define('DB_USER','root');
 define('DB_PASSWORD','');
 define('DB_HOST','localhost');
 
-$mysqli = new mysqli(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST);
+$mysqli = new mysqli(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST); //DB NAME
 
 date_default_timezone_set('Asia/Jakarta');
